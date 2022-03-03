4028c02b6582036655f6c2efe1ebf462d754c16f
AlgimantasMedeksa <algimantasmedeksa@gmail.com>
2022-03-03 15:58:29 +0200

    kursinis

---
+++ Biudzetas/Biudzetas/Islaidos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biudzetas
+{
+    class Islaidos
+    {
+        public string Menesis { get; set; }
+        public string Pavadinimas { get; set; }
+        public double Kiekis { get; set; }
+        public Islaidos (string menesis, string pavadinimas, double kiekis)
+        {
+            Menesis = menesis;
+            Pavadinimas = pavadinimas;
+            Kiekis = kiekis;
+        }
+    }
+}
---
+++ Biudzetas/Biudzetas/Pajamos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biudzetas
+{
+    public class Pajamos
+    {
+        public string Menesis { get; set; }
+        public string Pavadinimas { get; set; }
+        public double Kiekis { get; set; }
+        public Pajamos (string menesis, string pavadinimas, double kiekis)
+        {
+            Menesis = menesis;
+            Pavadinimas = pavadinimas;
+            Kiekis = kiekis;
+        }
+    }
+}
